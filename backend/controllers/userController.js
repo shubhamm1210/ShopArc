eb7b7e95294e467e55bd1445887bd1e87261c61c
Shubham Kumar <sk8340247116@gmail.com>
2026-02-17 19:14:15 +0530

    fix: proper case for userModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-import { User } from "../models/temp.js";
+import { User } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { verifyEmail } from "../emailVerify/verifyMail.js";
--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-import { User } from '../models/temp.js'
+import { User } from '../models/userModel.js'
 
 export const isAuthenticated = async(req,res,next)=>{
     try {
