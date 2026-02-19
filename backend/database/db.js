a93cecbf662eb0b24bbd71e5477ade32ebd0d598
Shubham Kumar <sk8340247116@gmail.com>
2026-02-19 19:40:26 +0530

    fix: remove duplicate db name from mongoose connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 const connectDB =async ()=>{
     try{
-        await mongoose.connect(`${process.env.MONGO_URI}/mini-ekart`)
+        await mongoose.connect(process.env.MONGO_URI)
+
         console.log('mongodb connected successfully');
         
 
